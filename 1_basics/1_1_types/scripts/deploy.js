ce210da5c99cdda195351863c63c78db113be767
Paul Brower <paul@team.casa>
2023-11-22 12:09:07 -0700

    all tests working

--- 1_basics/1_1_types/scripts/deploy.js
+++ 1_basics/1_1_types/scripts/deploy.js
@@ -10,10 +10,19 @@ async function main() {
   const myValContractDeploy = await hre.ethers.deployContract("SimpleVal");
 
   await myValContractDeploy.waitForDeployment();
+  const SimpleVal = await hre.ethers.getContractFactory("SimpleVal");
+
 
   console.log(
     `MyVal contract deployed to ${myValContractDeploy.target}`
   );
+  const simpleValInstance = SimpleVal.attach(myValContractDeploy.target)
+  const retVal = await simpleValInstance.getVal()
+  console.log(`ret val=${retVal}`)
+  if (retVal == 123) {
+    console.log('SUCCESS')
+  } else console.log(`FAILURE, was expecting 123, got ${retVal}`)
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
