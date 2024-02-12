8c1a73106447df7d8ec2c5da010f216482307e75
Paul Brower <paul@team.casa>
2024-02-12 15:45:10 -0700

    gen 1.10 scoping

---
+++ 1_basics/1_10_scoping/contracts/Contract.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract Contract {
+
+    constructor() {
+
+    }
+
+    function contractMethodReplace_Me() public view returns (string memory) {
+        return "OK";
+    }
+
+}
